7e9c49375ff85431f06bcff735afda068f36f7c1
Christoph Görn <goern@b4mad.net>
2011-01-18 14:53:04 +0100

    added GPLv3 header to file

--- src/test/java/org/b4mad/hornetq/PostToDlqTest.java
+++ src/test/java/org/b4mad/hornetq/PostToDlqTest.java
@@ -1,3 +1,19 @@
+/*	  This file is part of HornetQ-REST.
+ *
+ *   HornetQ-REST is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   HornetQ-REST is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.b4mad.nornetq;
 
 import org.jboss.resteasy.client.ClientRequest;
@@ -19,13 +35,13 @@ public class PostToDlqTest {
 	
 	@Test
 	public void testSuccessFirst() throws Exception {
-		ClientRequest request = new ClientRequest("http://localhost:8080/hornetq-rest/queues/jms.queue.DLQ");
+		ClientRequest request = new ClientRequest("http://localhost:8080/hornetq-rest/queues/jms.queue.Test");
 
 		ClientResponse response = request.head();
 		Assert.assertEquals("*****", 200, response.getStatus());
 		
-		Link sender = getLinkByTitle(null, response, "msg-create");
-		
+		Link sender = getLinkByTitle(null, response, "create");
+		System.out.println(response.toString());
 		// FIXME TODO complete this test
 	}
 }
